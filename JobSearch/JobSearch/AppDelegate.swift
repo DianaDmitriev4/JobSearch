4a9dd51191beaa04e9bec1b8cfaee26c0fe4fc81
DianaDmitriev4 <qwertyuiop25852@mail.ru>
2024-03-20 17:20:37 +0700

    no message

--- JobSearch/JobSearch/AppDelegate.swift
+++ JobSearch/JobSearch/AppDelegate.swift
@@ -24,15 +24,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var coordinator: AppCoordinator?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        //        
+        //
         //        let navController = UINavigationController()
         //        coordinator = AppCoordinator(navigationController: navController)
         //        coordinator?.start()
-        //        
+        //
         window = UIWindow(frame: UIScreen.main.bounds)
         if let window {
-//            window.rootViewController = ContainerViewController()
-                        window.rootViewController = TabBarController()
+            window.rootViewController = ContainerViewController()
+//            window.rootViewController = TabBarController()
             window.makeKeyAndVisible()
         }
         
